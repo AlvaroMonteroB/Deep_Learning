aa128277f2ee41be88fe63ca6fb687a74d14ba65
AlvaroMonteroB <alvaromonterob@outlook.com>
2024-03-04 19:31:35 -0600

    Inicio de practica

---
+++ Temas Selectos/P1/Practica1/main.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import matplotlib as plt
+from tensorflow.keras import layers
+
+
+optimizer=tf.keras.optimizers.Adam()
+
+dataframe=pd.read_csv(" ")
